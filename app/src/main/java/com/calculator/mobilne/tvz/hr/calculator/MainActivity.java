b980963eb24f2f3d3c1f70f85142878db9ec46cf
Marko Štuban <marko.stuban@ag04.com>
2018-11-22 13:43:59 +0100

    cnvert button variables to local

--- app/src/main/java/com/calculator/mobilne/tvz/hr/calculator/MainActivity.java
+++ app/src/main/java/com/calculator/mobilne/tvz/hr/calculator/MainActivity.java
@@ -8,10 +8,6 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button0, button1, button2, button3, button4, button5, button6,
-            button7, button8, button9, buttonAddition, buttonSubtract, buttonDivision,
-            buttonMultiply, button10, buttonC, buttonEquals;
-
     private EditText editText;
 
     private float mValueOne, mValueTwo;
@@ -23,25 +19,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button0 = findViewById(R.id.button0);
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
-        button4 = findViewById(R.id.button4);
-        button5 = findViewById(R.id.button5);
-        button6 = findViewById(R.id.button6);
-        button7 = findViewById(R.id.button7);
-        button8 = findViewById(R.id.button8);
-        button9 = findViewById(R.id.button9);
-        button10 = findViewById(R.id.button10);
-        buttonAddition = findViewById(R.id.buttonAddition);
-        buttonSubtract = findViewById(R.id.buttonSubtract);
-        buttonMultiply = findViewById(R.id.buttonMultiply);
-        buttonDivision = findViewById(R.id.buttonDivision);
-        buttonC = findViewById(R.id.buttonC);
-        buttonEquals = findViewById(R.id.buttonEquals);
+        Button button0 = findViewById(R.id.button0);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+        Button button10 = findViewById(R.id.button10);
+        Button buttonAddition = findViewById(R.id.buttonAddition);
+        Button buttonSubtract = findViewById(R.id.buttonSubtract);
+        Button buttonMultiply = findViewById(R.id.buttonMultiply);
+        Button buttonDivision = findViewById(R.id.buttonDivision);
+        Button buttonC = findViewById(R.id.buttonC);
+        Button buttonEquals = findViewById(R.id.buttonEquals);
         editText = findViewById(R.id.editText);
-
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
